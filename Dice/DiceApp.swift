e10f6643beda78bc8b6ece5739918435b6793cf8
Soroush Kami <soroushkami10@gmail.com>
2023-12-18 19:30:51 -0500

    Initial Commit

---
+++ Dice/DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceApp.swift
+//  Dice
+//
+//  Created by soroush kami on 2023-12-18.
+//
+
+import SwiftUI
+
+@main
+struct DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
